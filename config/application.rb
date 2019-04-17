04fc6e34e837768f4c7bf3eb2ad741ef6dbeb9ca
Anders <kiss7medical@gmail.com>
2019-04-17 14:07:45 +0200

    add seeds.rb file for 3x hotels

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Workdir
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+		config.i18n.available_locales = [:it_IT, :es_US, :en_UK]
+		config.i18n.default_locale = [:es_US]
   end
 end
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/index'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	  devise_for :users
+	  get 'pages/index'
 
-	root to: "hotels#index"
+	  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	resources :hotels
+		root to: "hotels#index"
+
+		resources :hotels
+	end
 end
+
+
